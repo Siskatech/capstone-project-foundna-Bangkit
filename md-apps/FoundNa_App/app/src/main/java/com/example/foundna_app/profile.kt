64736b961bc3256cef17c1c68f8d3b1e10704c7e
hanahanum <hanaanumnum@gmail.com>
2023-05-31 00:09:41 +0700

    Added my project

---
+++ md-apps/FoundNa_App/app/src/main/java/com/example/foundna_app/profile.kt
@@ -0,0 +1,13 @@
+package com.example.foundna_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+}
